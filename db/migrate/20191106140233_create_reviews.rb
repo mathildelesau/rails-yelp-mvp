a9f757b6603ebfb04906d44d8dbacac3e5173f75
mathilde lesauvage <lesauvage.mathilde94@gmail.com>
2019-11-06 18:07:33 +0100

    restaurant pas finis

---
+++ db/migrate/20191106140233_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :content
+      t.integer :rating
+      t.string :restaurant_id
+      t.references :restaurant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
